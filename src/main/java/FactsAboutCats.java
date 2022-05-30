013d106b05ec5a85edcb248d8e04d5967bbee3bd
Сергей Покровский <pokrovsky.s.n@gmail.com>
2022-05-30 16:16:56 +0300

    Initial commit

---
+++ src/main/java/FactsAboutCats.java
@@ -0,0 +1,27 @@
+public class FactsAboutCats {
+    private String id;
+    private String text;
+    private String type;
+    private String user;
+    private Integer upvotes;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+}
